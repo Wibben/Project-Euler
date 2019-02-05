abcde3e087ecba62be81196df83c94699cb36613
Wibben <bingr99@hotmail.com>
2019-02-04 22:47:44 -0500

    Problem 9 and reimplemented 7 using arrays and made it more of a general solution

--- p7.cpp
+++ p7.cpp
@@ -4,11 +4,10 @@
  * By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13,
  * we can see that the 6th prime is 13.
  *
- * What is the 10 001st prime number?
+ * What is the 10001st prime number?
  */
 
 #include <iostream>
-#include <vector>
 
 using namespace std;
 
@@ -17,27 +16,31 @@ int main ()
     // Method: Calculate prime numbers using pre-existing primes
     // If it is divisible by any found primes, it is not prime
     // if it is not divisible by any found primes, it is prime
-    vector<int> primes;
+	int nthPrime = 10001;
+	int foundPrimes;
     bool isPrime;
+	
+	int primes[nthPrime];
 
     // 2 is the first and only even prime number
-    primes.push_back(2);
+	foundPrimes = 1;
+    primes[0] = 2;
 
-    // Keep finding primes until the size of primes reaches 10001
+    // Keep finding primes until the number of found primes reaches 10001
     // Only need to look at odd numbers as all even numbers are divisible by 2
-    for(int i=3; primes.size()<10001; i+=2) {
+    for(int i=3; foundPrimes<nthPrime; i+=2) {
         isPrime = true;
         // Assuming the number is prime, if it is found to be non prime set the flag
         // to false and exit loop to check indivisibility
-        for(int j=0; j<primes.size() && isPrime; j++) {
+        for(int j=0; j<foundPrimes && isPrime; j++) {
             if(i%primes[j]==0) isPrime = false;
         }
 
-        if(isPrime) primes.push_back(i);
+        if(isPrime) primes[foundPrimes++] = i;
     }
 
     // Indexing starts from 0, so the 10001st prime is at index 10000
-    cout << "The 10001st prime is: " << primes[10000] << endl;
+    cout << "The 10001st prime is: " << primes[nthPrime-1] << endl;
 
     return 0;
 }
---
+++ p9.cpp
@@ -0,0 +1,43 @@
+/**
+ * Problem 9: Special Pythagorean Triplet
+ *
+ * A Pythagorean triplet is a set of three natural numbers,
+  * a < b < c, for which,
+ *       a^2 + b^2 = c^2
+ *
+ * For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
+ *
+ * There exists exactly one Pythagorean triplet for which
+ * a + b + c = 1000. Find the product abc.
+ */
+
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+int main ()
+{
+    // Method: Check all sets of triplets to see if they match the criteron:
+    // To run less loops, the one criteria is: a + b + sqrt(a*a + b*b) = abcSum
+    int abcSum;
+    vector<int> product;
+
+    abcSum = 1000;
+
+    // Loop and check for all a < b, find all products of abc that have the sum
+    for(int a=1; a<=abcSum; a++) {
+        for(int b=a; b<=abcSum; b++) {
+            if(a+b+sqrt(a*a+b*b) == 1000) product.push_back(a*b*sqrt(a*a+b*b));
+        }
+    }
+
+    // Output all product(s)
+    cout << "The products abc of all Pythagorean triples such that a+b+c = " << abcSum << " are:" << endl;
+    for(int i=0; i<product.size(); i++) {
+        cout << product[i] << endl;
+    }
+
+    return 0;
+}
