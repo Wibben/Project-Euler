5fdb7cf8d52d66584d19ebfa14161f9c02c08ab8
Wibben <bingr99@hotmail.com>
2021-07-26 21:59:06 -0400

    Problem 19 and 21

--- README.txt
+++ README.txt
@@ -3,25 +3,28 @@ problems can be found at projecteuler.net, each file corresponds to a specific p
 
 pX = problem X, can be accessed directly through projecteuler.net/problem=X
 
-pX	Name
+pX	    Name
 ----	------------
-1	Multiples of 3 and 5
-2	Even Fibonacci Numbers
-3	Largest Prime Factor
-4	Largest Palindrome Product
-5	Smallest Multiple
-6	Sum Square Difference
-7	10001st Prime
-8	Largest Product in a Series
-9	Special Pythagorean Triplet
-10	Summation of Primes
-11	Largest Product in a Grid
-12	Highly Divisible Triangular Number
-13	Large Sum
-14	Longest Collatz Sequence
-15	Lattice Paths
-16	Power Digit Sum
-17  Number Letter Counts
-18	Maximum Path Sum I
+1	    Multiples of 3 and 5
+2	    Even Fibonacci Numbers
+3	    Largest Prime Factor
+4	    Largest Palindrome Product
+5	    Smallest Multiple
+6	    Sum Square Difference
+7	    10001st Prime
+8	    Largest Product in a Series
+9	    Special Pythagorean Triplet
+10	    Summation of Primes
+11      Largest Product in a Grid
+12      Highly Divisible Triangular Number
+13  	Large Sum
+14  	Longest Collatz Sequence
+15  	Lattice Paths
+16  	Power Digit Sum
+17      Number Letter Counts
+18      Maximum Path Sum I
+19      Counting Sundays
 
-67	Maximum Path Sum II
+21      Amicable Numbers
+
+67	    Maximum Path Sum II
---
+++ p021.cpp
@@ -0,0 +1,56 @@
+/**
+ * Problem 5: Smallest Multiple
+ *
+ * Let d(n) be defined as the sum of proper divisors of n 
+ * (numbers less than n which divide evenly into n).
+ * If d(a) = b and d(b) = a, where a ≠ b, then a and b are 
+ * an amicable pair and each of a and b are called amicable numbers.
+ * 
+ * For example, the proper divisors of 220 are 1, 2, 4, 5, 10, 11, 20, 
+ * 22, 44, 55 and 110; therefore d(220) = 284. The proper divisors of 
+ * 284 are 1, 2, 4, 71 and 142; so d(284) = 220.
+ * 
+ * Evaluate the sum of all the amicable numbers under 10000.
+ */
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int get_divisor_sum(int num)
+{
+    int divisor_sum = 0;
+
+    for(int i=2; i<=num/2; i++) {
+        if(num%i == 0) divisor_sum += (i + num/i);
+    }
+
+    // Each divisor is counted twice, 1 and the number itself are also divisors, but number itself isn't counted
+    divisor_sum = divisor_sum/2 + 1;
+
+    return divisor_sum;
+}
+
+int main()
+{
+    vector<int> all_divisor_sum;
+    int sum_of_amical_numbers = 0;
+
+    all_divisor_sum.push_back(0); // Zero
+    // Calculate divisor sums
+    for(int i=1; i<10000; i++) {
+        all_divisor_sum.push_back(get_divisor_sum(i));
+    }
+
+    // Find amicable numbers
+    int val;
+    for(int i=1; i<10000; i++) {
+        val = all_divisor_sum[i];
+        if(val<10000 && val!=i && all_divisor_sum[val]==i) sum_of_amical_numbers += val;
+    }
+
+    cout << "Sum of all amicable numbers under 10000 is: " << sum_of_amical_numbers << endl;
+
+    return 0;
+}
